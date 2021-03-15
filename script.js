84a5048d7798e54aa693c5fc18dce62f8ee3169e
Clifford Morin <cemorin21@gmail.com>
2021-03-15 13:25:36 -0400

    added comments to js

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ var fiveDayForecast = $('#five-day-forecast');
 // My Key to Get access to the OpenWeather API 
 var apiKey = "629c5fc75bc1a656af7ff2d9281035d7";
 
-//Document 
+
+//Document Ready
 
 $(document).ready(function () {
 
@@ -28,10 +29,13 @@ $(document).ready(function () {
 var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
 
+//Current Weather API fetch function
 function currentWeather(city) {
+    //set url variable to url string
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial"
     
     fetch(url)
+     //if the response in not ok then throw back and rerun. If data is ok then return response.
      .then(function (response){
         if (!response.ok) {
             throw response.json();
@@ -40,16 +44,21 @@ function currentWeather(city) {
      })
      .then(function (data){
         console.log(data);
+        //sends latitude and longitude data to five day forecast function
         fiveDayForecast(data.coord.lat, data.coord.lon);
+        //change text to respective data call
         currentCity.text(data.name);
         currentTemp.text(data.main.temp);
         currentHumidity.text(data.main.humidity + "%");
         currentWindSpeed.text(data.wind.speed + "mph");
+        //changes img to the icon from open weather map data
         currentWeatherIcon.attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png")
      })
 }
 
+//Five Day forecast API fetch function
 function fiveDayForecast(lat, lon) {
+    //set url variable to url string
     var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey
     
     fetch(url)
@@ -61,10 +70,12 @@ function fiveDayForecast(lat, lon) {
           return response.json();
      })
      .then(function (data){
-         console.log(data);
+        console.log(data);
+
      })
 }
 
+//On click of the search btn take the value and send the input to currentWeather function
 searchCityBtn.on('click', function () {
     var input = searchCity.val()
     console.log(input); 
